cdc55de728a67cbc73b5f9b2fc190989842a44db
terminaldweller <thabogre@gmail.com>
2022-05-28 23:10:06 +0430

    wip

--- devourer/tests.sh
+++ devourer/tests.sh
@@ -5,3 +5,4 @@ curl -k -X GET "https://localhost:19019/mila/wiki?term=iommu&summary=none&audio=
 curl -k -X GET "https://localhost:19019/mila/reqs?url=https://www.ietf.org/rfc/rfc2865.txt&sourcetype=text"
 curl -k -X GET "https://localhost:19019/mila/pdf?feat=&url=https://www.rroij.com/open-access/mutation-testing-a-review-33-36.pdf"
 curl -k -X GET "https://localhost:19019/mila/pdf?feat=refs&url=https://www.rroij.com/open-access/mutation-testing-a-review-33-36.pdf"
+curl -k -X GET "https://localhost:19019/mila/pdf?feat=&url=https://www.rroij.com/open-access/mutation-testing-a-review-33-36.pdf&summarize=true"
